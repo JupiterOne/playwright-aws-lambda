ce8d5cf0d036880b4487609aa9027e21465a57da
Austin Kelleher <austin.kelleher@lifeomic.com>
2020-06-03 09:02:03 -0400

    Update playwright-core peerDependency

--- src/util/getMemorySize.ts
+++
@@ -1,8 +0,0 @@
-export default function getMemorySize() {
-  return parseInt(
-    process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE ||
-      process.env.FUNCTION_MEMORY_MB ||
-      '1024',
-    10
-  );
-}
