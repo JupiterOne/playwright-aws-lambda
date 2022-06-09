7edb80d7e50773f87dad2b87fb02925e657abc3f
Ian K Smith <smithki707@gmail.com>
2022-06-09 16:26:19 -0600

    Add Node.js 16.x pattern to environment check

--- src/util/isLambdaRuntimeEnvironment.ts
+++ src/util/isLambdaRuntimeEnvironment.ts
@@ -1,5 +1,8 @@
 export default function isLambdaRuntimeEnvironment(): boolean {
-  return ['AWS_Lambda_nodejs10.x', 'AWS_Lambda_nodejs12.x', 'AWS_Lambda_nodejs14.x'].includes(
-    process.env.AWS_EXECUTION_ENV as string
-  );
+  return [
+    'AWS_Lambda_nodejs10.x',
+    'AWS_Lambda_nodejs12.x',
+    'AWS_Lambda_nodejs14.x',
+    'AWS_Lambda_nodejs16.x',
+  ].includes(process.env.AWS_EXECUTION_ENV as string);
 }
